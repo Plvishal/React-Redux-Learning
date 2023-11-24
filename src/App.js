a7af8ea543a1df3106944b0fb7f11743e6455ae9
Vishal <vishalpal1501@gmail.com>
2023-11-24 13:27:21 +0530

    Creating provider  for the component to access the store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import { store } from './Redux/store';
+import { Provider } from 'react-redux';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Provider store={store}>{/* here pass the component */}</Provider>
+    </div>
+  );
 }
 
 export default App;
